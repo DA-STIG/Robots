0f7faa49df5d258a21430f37e7e7a877c9d5b4bc
Luci DaStig <ridex0101@gmail.com>
2020-02-23 17:59:41 -0600

    Updates

---
+++ precache-manifest.9371ef4f07103b0830866ead6ca43017.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1804f9f9179564a3e2ac7654bfbd07b",
+    "url": "/robots/index.html"
+  },
+  {
+    "revision": "85152bb0a9fc37bca838",
+    "url": "/robots/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8b3fddd962b8f15f62a8",
+    "url": "/robots/static/css/main.a70fcdac.chunk.css"
+  },
+  {
+    "revision": "85152bb0a9fc37bca838",
+    "url": "/robots/static/js/2.c3b2dec9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robots/static/js/2.c3b2dec9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b3fddd962b8f15f62a8",
+    "url": "/robots/static/js/main.d01147ee.chunk.js"
+  },
+  {
+    "revision": "fb7451c7fa6c3eca94a6",
+    "url": "/robots/static/js/runtime-main.f75a20b3.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robots/static/media/SEGA.12a3db8b.woff"
+  }
+]);
